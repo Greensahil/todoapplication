2d507664da4c1f0f2a10aeca9d1191aa26bc714d
unknown <ssharma2@svsu.edu>
2018-06-07 20:16:28 -0400

    change file structure

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express= require('express');
 var bodyParser=require('body-parser');
 
 var app=express();
-var mongoose=require('./db/mongoose')
-var {Todo}=require('./models/todo');
-var {User}=require('./models/user');
+var mongoose=require('./server/db/mongoose')
+var {Todo}=require('./server/models/todo');
+var {User}=require('./server/models/user');
 const port=process.env.PORT||3000;
 app.use(bodyParser.json())
 
